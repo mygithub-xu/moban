1c849c5aa8da6b917bc66947f2f2439cd8c86d54
徐晶波 <1967368657@qq.com>
2021-09-10 09:10:57 +0800

    ，，

--- htSystem/java/src/main/java/com/dhlg/utils/common/FileUtils.java
+++ htSystem/java/src/main/java/com/dhlg/utils/common/FileUtils.java
@@ -1,11 +1,14 @@
 package com.dhlg.utils.common;
 
+import cn.hutool.core.util.ZipUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.Enumeration;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
 
@@ -164,6 +167,62 @@ public class FileUtils {
         return dir.delete();
     }
 
+    public static void unZip(File srcFile, String destDirPath) throws RuntimeException {
+        long start = System.currentTimeMillis();
+        // 判断源文件是否存在
+        if (!srcFile.exists()) {
+            throw new RuntimeException(srcFile.getPath() + "所指文件不存在");
+        }
+        // 开始解压
+        ZipFile zipFile = null;
+        try {
+            zipFile = new ZipFile(srcFile);
+            Enumeration<?> entries = zipFile.entries();
+
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = (ZipEntry) entries.nextElement();
+                System.out.println("解压" + entry.getName());
+                // 如果是文件夹，就创建个文件夹
+                if (entry.isDirectory()) {
+                    String dirPath = destDirPath + "/" + entry.getName();
+                    File dir = new File(dirPath);
+                    dir.mkdirs();
+                } else {
+                    // 如果是文件，就先创建一个文件，然后用io流把内容copy过去
+                    File targetFile = new File(destDirPath + "/" + entry.getName());
+                    // 保证这个文件的父文件夹必须要存在
+                    if(!targetFile.getParentFile().exists()){
+                        targetFile.getParentFile().mkdirs();
+                    }
+                    targetFile.createNewFile();
+                    // 将压缩文件内容写入到这个文件中
+                    InputStream is = zipFile.getInputStream(entry);
+                    FileOutputStream fos = new FileOutputStream(targetFile);
+                    int len;
+                    byte[] buf = new byte[1024];
+                    while ((len = is.read(buf)) != -1) {
+                        fos.write(buf, 0, len);
+                    }
+                    // 关流顺序，先打开的后关闭
+                    fos.close();
+                    is.close();
+                }
+            }
+            long end = System.currentTimeMillis();
+            System.out.println("解压完成，耗时：" + (end - start) +" ms");
+        } catch (Exception e) {
+            throw new RuntimeException("unzip error from ZipUtils", e);
+        } finally {
+            if(zipFile != null){
+                try {
+                    zipFile.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 
     @Test
     public void mainfun(){
@@ -171,5 +230,20 @@ public class FileUtils {
         String storeSrc = "D:/bbbb";
         String zipName = "mytest05.zip";
         compressToZip(fileSrc,storeSrc,zipName);
+        File file = new File(storeSrc + "/" + zipName);
+        unZip(file,storeSrc);
+    }
+
+
+    @Test
+    public void cccccc(){
+//        String fileSrc = "D:/aaaaaa";
+//        String storeSrc = "D:/bbbb";
+//        String zipName = "mytest05.zip";
+//        compressToZip(fileSrc,storeSrc,zipName);
+//        ZipUtil.zip("d:/aaaaaa", "d:/bbbb/aaa.zip");
+        File unzip = ZipUtil.unzip("D:\\bbbb\\1.tgz", "D:\\aaaaaa");
+//        File file = new File(storeSrc + "/" + zipName);
+//        unZip(file,storeSrc);
     }
 }
