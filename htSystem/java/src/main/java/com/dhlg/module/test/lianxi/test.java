33e8a7b341a27c91fef3f7d4b41a60076fb43186
徐晶波 <1967368657@qq.com>
2021-09-13 09:21:35 +0800

    ，，

--- htSystem/java/src/main/java/com/dhlg/module/test/lianxi/test.java
+++ htSystem/java/src/main/java/com/dhlg/module/test/lianxi/test.java
@@ -7,19 +7,26 @@ public class test {
 
     public static void main(String[] args) {
 
-        String json = "{\"version\":\\\"v3\",\\\"coverPrdId\\\":4149,\\\"currentInsuredIndex\\\":0,\\\"holder\\\":{\\\"name\\\":\\\"\\\",\\\"gender\\\":\\\"M\\\",\\\"age\\\":30,\\\"birthday\\\":\\";
-
-        json = json.replaceAll("\\\\","");
-
-        System.out.println(json);//输出 Hello
-
-
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-        String format = sdf.format(new Date());
-        System.out.println(format.replaceAll("/",""));
+//        String json = "{\"version\":\\\"v3\",\\\"coverPrdId\\\":4149,\\\"currentInsuredIndex\\\":0,\\\"holder\\\":{\\\"name\\\":\\\"\\\",\\\"gender\\\":\\\"M\\\",\\\"age\\\":30,\\\"birthday\\\":\\";
+//
+//        json = json.replaceAll("\\\\","");
+//
+//        System.out.println(json);//输出 Hello
+//
+//
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+//        String format = sdf.format(new Date());
+//        System.out.println(format.replaceAll("/",""));
 
         int[] aa = {1,2,3};
-        String aaaaa = String.valueOf(aa[0]);
+         String aaaaa = String.valueOf(aa[0]);
+
+        int count = 0;
+        for (int i = 0; i < 10; i++) {
+            count = count++;
+
+        }
+        System.out.println("er:"+count);
     }
 
 }
