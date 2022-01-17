c7132cc9f9ec437211d73ae63eb154f8734321f8
whfch <1967368657@qq.com>
2022-01-17 17:30:31 +0800

    ，，，

--- htSystem/vue/src/config.js
+++ htSystem/vue/src/config.js
@@ -1,14 +1,18 @@
-let BASE_URL = ""; //这里是一个默认的url，可以没有
+let BASE_URL = ""
+let webSocker_URL = ""
 switch (process.env.NODE_ENV) {
     case 'development':
-        BASE_URL = "http://localhost:9000/moban/" //这里是本地的请求
-        // BASE_URL = "https://www.whfch.icu/moban/v1/" 
+        // BASE_URL = "http://localhost:9000/moban/" //这里是本地的请求
+        BASE_URL = "http://123.60.5.25/moban/"
+        webSocker_URL = "localhost:9000/moban/"
         break
     case 'test':
-        BASE_URL = "https://www.whfch.icu/moban/v1/" // 测试环境
+        BASE_URL = "http://123.60.5.25/moban/" // 测试环境
+        webSocker_URL = "123.60.5.25/moban/"
         break
     case 'production':
-        BASE_URL = "https://www.whfch.icu/moban/v1/" //生产环境
+        BASE_URL = "http://123.60.5.25/moban/" //生产环境
+        webSocker_URL = "123.60.5.25/moban/"
         break
 }
 
